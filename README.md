c8b34df9b8ec26d7971a4f25287bbc0c1d90704f
Roman Leshin <vorzee@gmail.com>
2025-01-18 18:14:53 +0500

    Подготовили проект для тестирования

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### Статус сборки
-[![Java CI with Gradle](https://github.com/vOrzee/aqa.hw_3/actions/workflows/action.yml/badge.svg)](https://github.com/vOrzee/aqa.hw_3/actions/workflows/action.yml)
+[![Java CI with Gradle](https://github.com/vOrzee/aqa.hw_3/actions/workflows/action.yml/badge.svg?branch=main)](https://github.com/vOrzee/aqa.hw_3/actions/workflows/action.yml)
---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:9999/v1");
+    }
+
+}
